0f1251bd775f0a68674fe02fe9a9faff9f60aaea
Vincent van Hees <v.vanhees@accelting.com>
2024-02-17 14:32:23 +0100

    changes to development script

--- inst/runGGIR.R
+++ inst/runGGIR.R
@@ -1,117 +1,109 @@
 rm(list = ls())
 dirR = dir("D:/Code/GGIR/R", full.names = TRUE)
+dirR = dirR[grep(pattern = "sysdata.rda", x = dirR, invert = TRUE, value = FALSE)]
 for (i in dirR) source(i)
 # library(GGIR)
 
 
 
 
-# Short recording
+# # Short recording
+# 
+# GGIR(datadir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/Jae/acc",
+#      outputdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/Jae",
+#      dataFormat = "actigraph_csv",
+#      mode = c(1:3),
+#      idloc = 6,
+#      overwrite = TRUE,
+#      do.parallel = FALSE,
+#      do.imp = FALSE,
+#      do.report = c(),
+#      windowsizes = c(10, 60, 900),
+#      threshold.lig = round(100 * (5/60), digits = 2),
+#      threshold.mod = round(2500 * (5/60), digits = 2),
+#      threshold.vig = round(10000 * (5/60), digits = 2),
+#      extEpochData_timeformat = "%m/%d/%Y %H:%M:%S",
+#      do.neishabouricounts = TRUE,
+#      acc.metric = "NeishabouriCount_x",
+#      HASPT.algo = "NotWorn",
+#      HASIB.algo = "NotWorn",
+#      do.visual = TRUE,
+#      includedaycrit = 2,
+#      includenightcrit = 2,
+#      visualreport = FALSE,
+#      outliers.only = FALSE,
+#      save_ms5rawlevels = TRUE,
+#      ignorenonwear = FALSE,
+#      HASPT.ignore.invalid = FALSE,
+#      save_ms5raw_without_invalid = FALSE
+# )
+# 
 
-GGIR(datadir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/Jae/acc",
-     outputdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/Jae",
-     dataFormat = "actigraph_csv",
-     mode = c(1:3),
-     idloc = 6,
-     overwrite = TRUE,
-     do.parallel = FALSE,
-     do.imp = FALSE,
-     do.report = c(),
-     windowsizes = c(10, 60, 900),
-     threshold.lig = round(100 * (5/60), digits = 2),
-     threshold.mod = round(2500 * (5/60), digits = 2),
-     threshold.vig = round(10000 * (5/60), digits = 2),
-     extEpochData_timeformat = "%m/%d/%Y %H:%M:%S",
-     do.neishabouricounts = TRUE,
-     acc.metric = "NeishabouriCount_x",
-     HASPT.algo = "NotWorn",
-     HASIB.algo = "NotWorn",
-     do.visual = TRUE,
-     includedaycrit = 2,
-     includenightcrit = 2,
-     visualreport = FALSE,
-     outliers.only = FALSE,
-     save_ms5rawlevels = TRUE,
-     ignorenonwear = FALSE,
-     HASPT.ignore.invalid = FALSE,
-     save_ms5raw_without_invalid = FALSE
-)
+# # Teun
+# 
+# 
+# GGIR(datadir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/Teun/Driestam/acc",
+#      outputdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/Teun/Driestam",
+#      dataFormat = "actigraph_csv",
+#      mode = c(1:3),
+#      idloc = 6,
+#      overwrite = FALSE,
+#      do.parallel = FALSE,
+#      do.imp = FALSE,
+#      do.report = c(2, 4, 5),
+#      windowsizes = c(10, 900, 3600),
+#      threshold.lig = round(100 * (5/60), digits = 2),
+#      threshold.mod = round(2500 * (5/60), digits = 2),
+#      threshold.vig = round(10000 * (5/60), digits = 2),
+#      extEpochData_timeformat = "%m/%d/%Y %H:%M:%S",
+#      do.neishabouricounts = TRUE,
+#      acc.metric = "NeishabouriCount_x",
+#      HASPT.algo = "NotWorn",
+#      HASIB.algo = "NotWorn",
+#      do.visual = TRUE,
+#      includedaycrit = 10,
+#      includenightcrit = 10,
+#      visualreport = FALSE,
+#      outliers.only = FALSE,
+#      save_ms5rawlevels = TRUE,
+#      ignorenonwear = FALSE,
+#      HASPT.ignore.invalid = FALSE,
+#      save_ms5raw_without_invalid = FALSE
+# )
 
-
-
-
-kkk
-
-
-# Teun
-
-
-GGIR(datadir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/Teun/Driestam/acc",
-     outputdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/Teun/Driestam",
-     dataFormat = "actigraph_csv",
-     mode = c(1:3),
-     idloc = 6,
-     overwrite = FALSE,
-     do.parallel = FALSE,
-     do.imp = FALSE,
-     do.report = c(2, 4, 5),
-     windowsizes = c(10, 900, 3600),
-     threshold.lig = round(100 * (5/60), digits = 2),
-     threshold.mod = round(2500 * (5/60), digits = 2),
-     threshold.vig = round(10000 * (5/60), digits = 2),
-     extEpochData_timeformat = "%m/%d/%Y %H:%M:%S",
-     do.neishabouricounts = TRUE,
-     acc.metric = "NeishabouriCount_x",
-     HASPT.algo = "NotWorn",
-     HASIB.algo = "NotWorn",
-     do.visual = TRUE,
-     includedaycrit = 10,
-     includenightcrit = 10,
-     visualreport = FALSE,
-     outliers.only = FALSE,
-     save_ms5rawlevels = TRUE,
-     ignorenonwear = FALSE,
-     HASPT.ignore.invalid = FALSE,
-     save_ms5raw_without_invalid = FALSE
-)
-
-
-
-# DK
-
-
-GGIR(datadir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/DKtestdata/ACC",
-     outputdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/DKtestdata",
-     dataFormat = "actigraph_csv",
-     mode = c(1:5),
-     overwrite = TRUE,
-     do.report = c(2, 4, 5),
-     windowsizes = c(15, 900, 3600),
-     threshold.in = round(100 * (5/60), digits = 2),
-     threshold.mod = round(2500 * (5/60), digits = 2),
-     threshold.vig = round(10000 * (5/60), digits = 2),
-     extEpochData_timeformat = "%m/%d/%Y",
-     do.neishabouricounts = TRUE,
-     acc.metric = "NeishabouriCount_x",
-     HASPT.algo = "NotWorn",
-     HASIB.algo = "NotWorn",
-     do.visual = TRUE,
-     includedaycrit = 10,
-     includenightcrit = 10,
-     visualreport = FALSE,
-     outliers.only = FALSE,
-     save_ms5rawlevels = TRUE,
-     ignorenonwear = FALSE,
-     HASPT.ignore.invalid = FALSE,
-     save_ms5raw_without_invalid = FALSE
-)
+# # DK
+# GGIR(datadir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/DKtestdata/ACC",
+#      outputdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/DKtestdata",
+#      dataFormat = "actigraph_csv",
+#      mode = c(1:5),
+#      overwrite = TRUE,
+#      do.report = c(2, 4, 5),
+#      windowsizes = c(15, 900, 3600),
+#      threshold.in = round(100 * (5/60), digits = 2),
+#      threshold.mod = round(2500 * (5/60), digits = 2),
+#      threshold.vig = round(10000 * (5/60), digits = 2),
+#      extEpochData_timeformat = "%m/%d/%Y",
+#      do.neishabouricounts = TRUE,
+#      acc.metric = "NeishabouriCount_x",
+#      HASPT.algo = "NotWorn",
+#      HASIB.algo = "NotWorn",
+#      do.visual = TRUE,
+#      includedaycrit = 10,
+#      includenightcrit = 10,
+#      visualreport = FALSE,
+#      outliers.only = FALSE,
+#      save_ms5rawlevels = TRUE,
+#      ignorenonwear = FALSE,
+#      HASPT.ignore.invalid = FALSE,
+#      save_ms5raw_without_invalid = FALSE
+# )
 
 
-kkk
+# kkk
 
 GGIR(datadir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/acc_playce",
      outputdir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing",
-     mode = c(5),
+     mode = c(1:5),
      overwrite = TRUE,
      do.report = c(),
      windowsizes = c(5, 900, 3600),
@@ -127,28 +119,28 @@ GGIR(datadir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/acc_play
      save_ms5rawlevels = TRUE,
      save_ms5raw_without_invalid = FALSE
 )
-GGIR(datadir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/gps_playce/41023_B37-20160722.csv",
-     outputdir = "D:/Dropbox/Work/sharedfolder/DATA/BohmerRotterdam2023/actigraph",
-     dataFormat = "actigraph_csv",
-     mode = c(1:5),
-     overwrite = FALSE,
-     do.report = c(2),
-     windowsizes = c(1, 900, 3600),
-     threshold.in = round(100 * (5/60), digits = 2),
-     threshold.mod = round(2500 * (5/60), digits = 2),
-     threshold.vig = round(10000 * (5/60), digits = 2),
-     extEpochData_timeformat = "%m/%d/%Y",
-     do.neishabouricounts = TRUE,
-     acc.metric = "NeishabouriCount_x",
-     HASPT.algo = "NotWorn",
-     HASIB.algo = "NotWorn",
-     do.visual = TRUE,
-     includedaycrit = 10,
-     includenightcrit = 10,
-     visualreport = FALSE,
-     outliers.only = FALSE,
-     save_ms5rawlevels = TRUE,
-     ignorenonwear = FALSE,
-     HASPT.ignore.invalid = FALSE,
-     save_ms5raw_without_invalid = FALSE
-)
+# GGIR(datadir = "D:/Dropbox/Work/sharedfolder/DATA/Habitus/GPSprocessing/gps_playce/41023_B37-20160722.csv",
+#      outputdir = "D:/Dropbox/Work/sharedfolder/DATA/BohmerRotterdam2023/actigraph",
+#      dataFormat = "actigraph_csv",
+#      mode = c(1:5),
+#      overwrite = FALSE,
+#      do.report = c(2),
+#      windowsizes = c(1, 900, 3600),
+#      threshold.in = round(100 * (5/60), digits = 2),
+#      threshold.mod = round(2500 * (5/60), digits = 2),
+#      threshold.vig = round(10000 * (5/60), digits = 2),
+#      extEpochData_timeformat = "%m/%d/%Y",
+#      do.neishabouricounts = TRUE,
+#      acc.metric = "NeishabouriCount_x",
+#      HASPT.algo = "NotWorn",
+#      HASIB.algo = "NotWorn",
+#      do.visual = TRUE,
+#      includedaycrit = 10,
+#      includenightcrit = 10,
+#      visualreport = FALSE,
+#      outliers.only = FALSE,
+#      save_ms5rawlevels = TRUE,
+#      ignorenonwear = FALSE,
+#      HASPT.ignore.invalid = FALSE,
+#      save_ms5raw_without_invalid = FALSE
+# )
